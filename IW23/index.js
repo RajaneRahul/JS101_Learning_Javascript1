406c10abae5bc3d47f799761e00c94a9b1cdca3d
RajaneRahul <rahulrajane177@gmail.com>
2022-10-11 18:28:14 +0530

    introduction of js2

---
+++ IW23/index.js
@@ -0,0 +1,18 @@
+let database_user = "abc@gmail.com";
+let database_pass = "123abc";
+
+//for login
+let user = "abc@gmail.com";
+
+let pass = "123abc";
+
+if (database_user == user) {
+  
+  if (database_pass  == pass) {
+    
+    console.log("login Successfull");
+    
+  } else{
+   console.log("incorrect password"); 
+  }  
+}    
